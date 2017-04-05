981d9a171a2ee6b13fcba1f354e1843585f35872
Joel Mitchell <me@joelmitchell.co.uk>
2017-04-05 10:24:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Cofoundry.Samples.Menus
+
+A bare website showing various examples of creating menus from page routes.
+
+*Work in progress, see related [issue](https://github.com/cofoundry-cms/cofoundry/issues/89)*
+
+
---
+++ src/Cofoundry.Samples.Menus/Models/FooterMenuViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Cofoundry.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cofoundry.Samples.Menus
+{
+    public class FooterMenuViewModel
+    {
+        public IEnumerable<PageRoute> PageRoutes { get; set; }
+    }
+}
